e873edf4cb26edd52f1997bd59045f8ff9231069
Ivan Yelizariev // IEL <yelizariev@itpp.dev>
2020-12-10 15:22:45 +0100

    Initial commit

---
+++ dummy/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+{
+    "name": """Dummy module""",
+    "summary": """The module do nothing""",
+    "category": "Hidden",
+    "images": [],
+    "version": "1.0.1",
+    "application": False,
+
+    "author": "Ivan Yelizariev",
+    "depends": [
+    ],
+    "external_dependencies": {"python": [], "bin": []},
+    "data": [
+    ],
+    "qweb": [
+    ],
+    "demo": [
+    ],
+
+    "post_load": None,
+    "pre_init_hook": None,
+    "post_init_hook": None,
+
+    "auto_install": False,
+    "installable": True,
+}
